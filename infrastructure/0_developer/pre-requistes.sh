f7895da781e865ea29a841a480571958d468eb1c
Chris Kemp <chris.kemp3000@gmail.com>
2024-02-22 23:53:54 +0000

    Updated pre-reqs

--- infrastructure/0_developer/pre-requistes.sh
+++ infrastructure/0_developer/pre-requistes.sh
@@ -5,8 +5,12 @@
 
 brew update || { echo "Brew update failed"; exit 1; }
 brew install asdf || { echo "Brew failed to install asdf"; exit 1; }
+brew install gh || { echo "Brew failed to install gh"; exit 1; }
+brew install awscli || { echo "Brew failed to install awscli"; exit 1; }
 
-asdf plugin-add terraform || { echo "Failed to add terraform plugin"; exit 1; }
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install terraform
+asdf plugin-add terraform || { echo "Failed to add terraform plugin"; exit 1; }
 asdf install terraform || { echo "Failed to install terraform version"; exit 1; }
